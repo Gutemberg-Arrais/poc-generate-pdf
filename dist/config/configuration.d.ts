5c2947a0046ce536e61e209f45be79f907af8f62
Gutemberg-Arrais <arraisgutemberg@gmail.com>
2022-12-31 17:29:45 -0300

    Removido dist

--- dist/config/configuration.d.ts
+++
@@ -1,7 +0,0 @@
-declare const _default: () => {
-    projectId: any;
-    private_key: any;
-    client_email: any;
-    mediaBucket: any;
-};
-export default _default;
--- dist/config/configuration.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = () => ({
-    projectId: process.env.PROJECT_ID,
-    private_key: process.env.PRIVATE_KEY.replace(/\\n/gm, '\n'),
-    client_email: process.env.CLIENT_EMAIL,
-    mediaBucket: process.env.STORAGE_MEDIA_BUCKET,
-});
-//# sourceMappingURL=configuration.js.map
